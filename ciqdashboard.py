78b082e92070abbd910dfde698e4a2721655cb55
RyanJo3 <127467207+RyanJo3@users.noreply.github.com>
2024-09-20 10:16:54 +0900

    Update ciqdashboard.py

--- ciqdashboard.py
+++ ciqdashboard.py
@@ -123,12 +123,12 @@ def get_latest_file(folder_path):
     folder = Path(folder_path)
     files = list(folder.glob('*.xlsx'))
     print(f"Found files: {files}")  # Debugging message
-    
+    return files
     if not files:
         return None
     latest_file = max(files, key=os.path.getctime)
     print(f"Latest file: {latest_file}")  # Debugging message
-    return files
+    return latest_file
 
 
 # In[6]:
